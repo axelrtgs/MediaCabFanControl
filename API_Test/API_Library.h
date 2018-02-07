6214868c30d6d27fedebf28b234d8e3aeb55f9bb
axelrtgs <jamie@axelrtgs.com>
2018-02-06 22:49:47 -0500

    merge alex changes (#1)
    * move stuff into class, use enum
    
    * use switch
    
    * simplify handle calls
    
    * simplify construction

--- API_Test/API_Library.h
+++ API_Test/API_Library.h
@@ -1,24 +1,15 @@
 #ifndef API_Library_h
 #define API_Library_h
 
-#define UNDEFINED 0
-#define JSON      1
-#define REST      2
-
-uint8_t api_call_type      = UNDEFINED;
-String  api_input          = "";
-bool    api_input_complete = false;
-
 class API_Library {
 
+enum API_TYPE { UNDEFINED, JSON, REST };
+
+API_TYPE api_call_type      = UNDEFINED;
+String   api_input          = "";
+bool     api_input_complete = false;
+
 public:
-
-API_Library() {
-  api_call_type      = UNDEFINED;
-  api_input          = "";
-  api_input_complete = false;
-}
-
 
 void setup() {
   Serial.begin(9600);
@@ -28,36 +19,32 @@ void setup() {
 void loop() {
   if (api_input_complete) {
     Serial.println(api_input);
-
     reset_state(false);
   }
 }
 
 void serialEvent() {
   while (Serial.available()) {
-    char inChar = (char)Serial.read();
+    char inChar = (char) Serial.read();
 
-    if(api_call_type == UNDEFINED) {
-      detect_api_type(inChar);
-    } else if(api_call_type == JSON) {
-      api_input += inChar;
-      if (inChar=='}') {
-        api_input += '\0';
-
-        handle_json_msg(api_input);
-
-        reset_state(true);
-      }
-    } else if(api_call_type == REST) {
-      if (inChar=='\n') {
-        api_input += '\0';
-
-        handle_rest_msg(api_input);
-
-        reset_state(true);
-      } else {
+    switch (api_call_type) {
+      case UNDEFINED:
+        detect_api_type(inChar);
+      case JSON:
         api_input += inChar;
-      }
+        if (inChar == '}') {
+          api_input += '\0';
+          handle_json_msg();
+          reset_state(true);
+        }
+      case REST:
+        if (inChar == '\n') {
+          api_input += '\0';
+          handle_rest_msg();
+          reset_state(true);
+        } else {
+          api_input += inChar;
+        }
     }
   }
 }
@@ -67,19 +54,17 @@ private:
 void reset_state(bool input_complete) {
   api_call_type           = UNDEFINED;
   api_input_complete      = input_complete;
-  if(!input_complete) {
+  if (!input_complete) {
     api_input             = "";
   }
 }
 
-void handle_json_msg(String encoded_json) {
-  api_input = encoded_json;
+void handle_json_msg() {
   Serial.print("JSON: ");
   Serial.println(api_input);
 }
 
-void handle_rest_msg(String rest_msg) {
-  api_input = rest_msg;
+void handle_rest_msg() {
   Serial.print("REST: ");
   Serial.println(api_input);
 }
